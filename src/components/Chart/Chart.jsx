42b514f1c698e8a06ffe64ac7eb75f751d408fc0
mertcancet <61883804+mertcancet@users.noreply.github.com>
2020-05-10 20:01:23 +0300

    youtube

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,80 @@
+import React,{useState, useEffect} from "react";
+import { fetchDailyData } from '../../api';
+import {Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
+
+
+const Chart =({data: {confirmed,deaths,recovered},country})=>{
+    const [dailyData,setDailyData]=useState([]);
+    useEffect(()=>{
+        const fetchAPI =async()=>{
+           setDailyData(await fetchDailyData());
+        }
+
+        
+        fetchAPI();
+    },[]);
+
+    const lineChart =(
+        dailyData.length
+       ?
+       ( <Line
+        data={{
+            labels: dailyData.map(({date})=>date),
+            datasets:[{
+                data: dailyData.map(({confirmed})=>confirmed),
+                label:'Toplam Vaka Sayısı',
+                borderColor: '#3333ff',
+                fill:true,
+            },{
+                data: dailyData.map(({deaths})=>deaths),
+                label:'Toplam Vefat Sayısı',
+                borderColor: 'red',
+                backgroundColor:'rgba(255,0,0,0.5)',
+                fill:true,
+            }],
+        }}
+        />):null
+
+    );
+
+    const barChart =(
+        confirmed
+        ?(<Bar 
+            data={{
+                labels:['Toplam Vaka Sayısı','Toplam İyileşen Hasta Sayısı','Toplam Vefat Sayısı'],
+                datasets:[{
+                    label:'People',
+                    backgroundColor:[
+                        'rgba(0,0,255,0.5)',
+                        'rgba(0,255,0,0.5)',
+                        'rgba(255,0,0,0.5)',
+                        
+                    ],
+                    data:[confirmed.value,recovered.value,deaths.value]                    
+                    
+                }]
+
+            }}
+            options={{
+                legend:{display: false},
+                title:{display:true, text:` ${country} 'de son durum`},
+
+            }}
+            
+            />
+            
+            ):null
+
+    )
+
+
+
+    return(
+        <div className={styles.container}>
+        {country?barChart:lineChart}
+        </div>
+    )
+}
+export default Chart;
